809d32941f6bbff9a3fae9f1fd38c5be6cd52c71
Shykial <szymon.jeziorski@interia.pl>
2022-09-03 16:58:02 +0200

    added aggregation method for retrieving domain request details along with extractingDetails

--- src/main/kotlin/com/shykial/kScraperCore/repository/DomainRequestDetailsRepository.kt
+++ src/main/kotlin/com/shykial/kScraperCore/repository/DomainRequestDetailsRepository.kt
@@ -1,6 +1,8 @@
 package com.shykial.kScraperCore.repository
 
 import com.shykial.kScraperCore.model.entity.DomainRequestDetails
+import com.shykial.kScraperCore.model.entity.ExtractingDetails
+import org.springframework.data.mongodb.repository.Aggregation
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
@@ -9,4 +11,28 @@ interface DomainRequestDetailsRepository : CoroutineCrudRepository<DomainRequest
     suspend fun findByDomainName(domainName: String): DomainRequestDetails?
 
     suspend fun findByRequestTimeoutInMillis(timeoutInMillis: Int): DomainRequestDetails?
+
+    @Aggregation(
+        pipeline = [
+            "{ \$match: { domainName: ?0 } }",
+            "{ \$project: { domainRequestDetailsId: { \$toString: '\$_id' }, domainRequestDetails: '\$\$ROOT' } }",
+            """
+            { 
+              ${'$'}lookup: {
+                from: "extractingDetails",
+                localField: "domainRequestDetailsId",
+                foreignField: "domainId",
+                as: "extractingDetails"
+              }
+            }
+            """,
+            "{ \$addFields:  { extractingDetails: { \$first: '\$extractingDetails' } } }"
+        ]
+    )
+    suspend fun findAggregationMatch(domainName: String): AggregatedMatch?
 }
+
+data class AggregatedMatch(
+    val domainRequestDetails: DomainRequestDetails,
+    val extractingDetails: ExtractingDetails?
+)
